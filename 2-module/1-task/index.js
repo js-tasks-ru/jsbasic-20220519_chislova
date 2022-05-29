11748acc50223f1206006860a6adfa881cf91ead
Chislova <44863427+Chislova@users.noreply.github.com>
2022-05-29 14:07:55 +0300

    module 2 task 1, task 2

--- 2-module/1-task/index.js
+++ 2-module/1-task/index.js
@@ -1,3 +1,12 @@
 function sumSalary(salaries) {
-  // ваш код...
+  let sum = 0;
+  for (let key in salaries) {
+    if (typeof (salaries[key]) === "number"
+      && salaries[key] !== Infinity
+      && salaries[key] !== -Infinity
+      && !!salaries[key] !== !!NaN) {
+      sum += salaries[key];
+    }
+  }
+  return sum;
 }
