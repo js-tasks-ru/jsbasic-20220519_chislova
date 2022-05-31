ac77bf6090f25642f7055b4d2ecc0fdc73a8e84d
Chislova <44863427+Chislova@users.noreply.github.com>
2022-05-31 10:04:03 +0300

    task calculator

--- 2-module/3-task/index.js
+++ 2-module/3-task/index.js
@@ -1,5 +1,15 @@
 let calculator = {
-  // ваш код
+
+  read(var1, var2) {
+    this.variable1 = var1;
+    this.variable2 = var2;
+  },
+  sum() {
+    return this.variable1 + this.variable2;
+  },
+  mul() {
+    return this.variable1 * this.variable2;
+  }
 };
 
 // НЕ УДАЛЯТЬ СТРОКУ, НУЖНА ДЛЯ ПРОВЕРКИ
